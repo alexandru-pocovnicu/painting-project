9dbd90b4c4720465468d39b0821f2b06bb686d28
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-07-04 20:17:03 +0100

    added for loop

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-const walls=document.getElementsByClassName('.walls');
+const walls=document.getElementsByClassName('walls');
+for (let i=0;i<walls.length;i++){
+    walls[i].addEventListener("click", function(){
+        
+    })
+}
